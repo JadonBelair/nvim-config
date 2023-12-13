df8444b9220c39b50f4dfb55ae88548b951570e9
Jadon Belair <46850171+JadonBelair@users.noreply.github.com>
2023-12-13 11:18:24 -0500

    overwrite certain keybinds to work with rust-tools

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,18 +1,18 @@
 local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "<Leader>k", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 require('mason').setup({})
@@ -50,16 +50,31 @@ cmp.setup({
 })
 
 lsp_zero.set_sign_icons({
-  error = 'E',
-  warn = 'W',
-  hint = 'H',
-  info = 'I'
+	error = 'E',
+	warn = 'W',
+	hint = 'H',
+	info = 'I'
 })
 
 
 lsp_zero.setup()
-require('rust-tools').setup()
+
+local rt = require('rust-tools')
+rt.setup({
+	server = {
+		capabilities = require("cmp_nvim_lsp").default_capabilities(),
+		on_attach = function(_, bufnr)
+			vim.keymap.set("n", "<Leader>k", rt.hover_actions.hover_actions, { buffer = bufnr })
+			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+		end,
+	},
+	tools = {
+		hover_actions = {
+			auto_focus = true
+		},
+	},
+})
 
 vim.diagnostic.config({
-    virtual_text = true
+	virtual_text = true
 })
