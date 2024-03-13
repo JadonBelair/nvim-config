216f7ed8ffaaf745c82d72842cc44f21f2249482
Jadon Belair <46850171+JadonBelair@users.noreply.github.com>
2024-03-12 23:23:17 -0400

    added keybind for moving line up/down

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next)
 vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev)
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
