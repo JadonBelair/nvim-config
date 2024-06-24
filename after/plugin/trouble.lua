dbf5827fb6e7c5b9119520cd47178f365c218048
Jadon <46850171+JadonBelair@users.noreply.github.com>
2024-06-23 23:30:09 -0400

    fixed trouble not opening

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,11 +1,18 @@
 local trouble = require('trouble')
 
 trouble.setup({
-	icons = false,
-    fold_open = "v",
-    fold_closed = ">",
-    indent_lines = false,
+	icons = {
+		indent = {
+			fold_open = "v",
+			fold_closed = ">",
+		},
+		folder_open = "v",
+		folder_closed = ">",
+	},
+    indent_guides = false,
 	use_diagnostic_signs = true,
+	warn_no_results = false,
+	open_no_results = true,
 })
 
-vim.keymap.set("n", "ds", trouble.toggle)
+vim.keymap.set("n", "ds", "<Cmd>Trouble diagnostics toggle focus=true<CR>")
