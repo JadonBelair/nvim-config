14948ed69ce5d943536652d2fddf5c7107cd9e64
Jadon <46850171+JadonBelair@users.noreply.github.com>
2024-09-26 21:56:06 -0400

    updated lsp-zero to 4.x

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -25,7 +25,7 @@ return require('packer').startup(function(use)
 
 	use {
 		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
+		branch = 'v4.x',
 		requires = {
 			-- LSP Support
 			{'neovim/nvim-lspconfig'},
