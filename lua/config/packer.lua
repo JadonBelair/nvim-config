e9fb7d89a2ab5e6b6e1cbb57b02f9bd29c80f580
Jadon Belair <46850171+JadonBelair@users.noreply.github.com>
2026-03-04 17:50:56 -0500

    switched to latest version of telescope

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -10,7 +10,7 @@ return require('packer').startup(function(use)
 	use "nvim-lua/plenary.nvim"
 
 	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
